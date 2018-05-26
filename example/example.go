dacafb1224f5207fa68bfebe12c3d7b90115c136
Toshiki Tomihira <toshiki0714@icloud.com>
2018-05-26 11:37:19 +0900

    update example for new method

--- example/example.go
+++ example/example.go
@@ -21,9 +21,9 @@ func main() {
 	// Do jobs without params
 	gocron.Every(1).Second().Do(task)
 	gocron.Every(2).Seconds().Do(task)
-	gocron.Every(1).Minute().Do(task)
-	gocron.Every(2).Minutes().Do(task)
-	gocron.Every(1).Hour().Do(task)
+	gocron.EveryWithId(1, 1).Minute().Do(task) //first parameter is job ID
+	gocron.EveryWithId(2, 2).Minutes().Do(task)
+	gocron.EveryOnlyId(3).Hour().Do(task) //first parameter is job ID and interval is 1
 	gocron.Every(2).Hours().Do(task)
 	gocron.Every(1).Day().Do(task)
 	gocron.Every(2).Days().Do(task)
@@ -40,7 +40,14 @@ func main() {
 	_, time := gocron.NextRun()
 	fmt.Println(time)
 
-	// gocron.Remove(task)
+	// get all jobs list
+	jobs := gocron.AllJobs()
+	for i, job := range jobs {
+		fmt.Println(i, job.RunTime())
+	}
+
+	// gocron.RemoveFromFunc(task)
+	// gocron.RemoveFromId(3)
 	// gocron.Clear()
 
 	// function Start start all the pending jobs
